5b3e724e1617d464aaccbbbd126879b55719cb70
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-10-17 16:25:17 -0400

    UserList -> List

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { css } from 'glamor';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
 import colors from 'common/colors';
 import { getUsers } from 'services';
 import Nav from 'components/Nav';
-
-import UserList from './UserList';
+import List from 'components/List';
 
 const styles = {
   container: {
@@ -15,7 +12,24 @@ const styles = {
     width: '100%',
     flexWrap: 'initial',
   },
+  list: {
+    minWidth: 300,
+    background: colors.lightGrey,
+    borderRight: `1px solid ${colors.grey}`,
+    padding: '0 30px 20px',
+    overflowY: 'auto',
+    flex: 'none',
+  },
   content: {},
+};
+
+const User = ({ item: { userName, email }, style, ...props }) => {
+  return (
+    <div style={{ padding: '20px 0', ...style }} {...props}>
+      <div style={{ fontSize: 20, paddingBottom: 5 }}>{userName}</div>
+      <div>{email}</div>
+    </div>
+  );
 };
 
 const Content = ({ data }) => {
@@ -31,7 +45,12 @@ export default class extends React.Component {
     return (
       <div className={`row ${css(styles.container)}`}>
         <Nav />
-        <UserList onSelect={currentUser => this.setState({ currentUser })} />
+        <List
+          Component={User}
+          getKey={item => item.id}
+          getData={getUsers}
+          onSelect={currentUser => this.setState({ currentUser })}
+        />
         {this.state.currentUser && <Content data={this.state.currentUser} />}
       </div>
     );
