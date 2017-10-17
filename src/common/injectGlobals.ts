4912686a50d79afe9df61dc8fbe187e56bbe35a3
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-10-17 14:13:45 -0400

    add debug and locale

--- src/common/injectGlobals.ts
+++ src/common/injectGlobals.ts
@@ -1,3 +1,7 @@
 import 'semantic-ui-css/semantic.min.css';
 
+localStorage.setItem('debug', process.env.REACT_APP_DEBUG || ''); // manually set because CRA doesn't allow arbitrary env variable names.
+const debug = require('debug') as Function;
+global.log = debug('app');
+
 export const apiRoot = process.env.REACT_APP_API;
