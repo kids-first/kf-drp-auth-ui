7a268608f7cdfb5f71e2387ef2fe2399c41d7de4
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-10-19 16:52:49 -0400

    format fields

--- src/components/Users/Content.tsx
+++ src/components/Users/Content.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { css } from 'glamor';
 import _ from 'lodash';
 import { Table } from 'semantic-ui-react';
+import { format } from 'date-fns';
 
 const styles = {
   container: {
@@ -9,6 +10,7 @@ const styles = {
   },
 };
 
+const DATE_KEYS = ['createdAt', 'lastLogin'];
 export default ({ data }) => {
   return (
     <div className={`${css(styles.container)}`}>
@@ -27,11 +29,30 @@ export default ({ data }) => {
             'id',
           ].map(key => {
             return (
-              <Table.Row key={key}>
-                <Table.Cell style={{ fontSize: '0.7em', border: 'none' }}>
+              <Table.Row key={key} style={{ verticalAlign: 'baseline' }}>
+                <Table.Cell
+                  style={{
+                    fontSize: '0.65em',
+                    border: 'none',
+                    textAlign: 'right',
+                  }}
+                >
                   {_.upperCase(key)}
                 </Table.Cell>
-                <Table.Cell style={{ border: 'none' }}>{data[key]}</Table.Cell>
+                <Table.Cell style={{ border: 'none' }}>
+                  {data[key] ? (
+                    DATE_KEYS.indexOf(key) >= 0 ? (
+                      format(data[key], 'MMMM Do YYYY [a]t h:mmA')
+                    ) : (
+                      data[key]
+                    )
+                  ) : (
+                    <div style={{ opacity: 0.4, fontStyle: 'italic' }}>
+                      empty
+                    </div>
+                  )}
+                  {}
+                </Table.Cell>
               </Table.Row>
             );
           })}
