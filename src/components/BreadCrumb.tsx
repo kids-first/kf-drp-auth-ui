a2e0a77443cca881b14d42fee6eead51565064d6
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-11-08 16:06:32 -0500

    colours

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Breadcrumb } from 'semantic-ui-react';
 import Aux from 'components/Aux';
 import { NavLink } from 'react-router-dom';
+import { css } from 'glamor';
 
 export default ({ path }) => (
-  <Breadcrumb style={{ padding: 20, background: '#e7e7e7' }}>
+  <Breadcrumb
+    style={{ padding: '10px 20px', background: 'rgb(33, 133, 208)', textTransform: 'capitalize' }}
+  >
     {path
       .split('/')
       .filter(Boolean)
@@ -14,9 +17,25 @@ export default ({ path }) => (
         return (
           <Aux key={crumb}>
             <Breadcrumb.Section active={isLast}>
-              {isLast ? crumb : <NavLink to={`/${arr.slice(0, i + 1).join('/')}`}>{crumb}</NavLink>}
+              {isLast ? (
+                <span className={`${css({ color: '#eaf3f9 !important', fontWeight: 400 })}`}>
+                  {crumb}
+                </span>
+              ) : (
+                <NavLink
+                  className={css({ color: '#fff !important', opacity: 0.8 })}
+                  to={`/${arr.slice(0, i + 1).join('/')}`}
+                >
+                  {crumb}
+                </NavLink>
+              )}
             </Breadcrumb.Section>
-            {!isLast && <Breadcrumb.Divider />}
+            {!isLast && (
+              <Breadcrumb.Divider
+                style={{ margin: '0px 0.4em 0 0.5em' }}
+                className={`${css({ color: '#fff !important' })}`}
+              />
+            )}
           </Aux>
         );
       })}
